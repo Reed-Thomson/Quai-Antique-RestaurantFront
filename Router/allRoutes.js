7bcf9de3d37173cf04897ffb8484771cb841a7e7
ReedThomson <beydycoulibaly1@gmail.com>
2026-01-12 10:19:03 +0100

    Ajout du footer

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,13 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "Galerie", "/pages/galerie.html"),
+    new Route("/carte", "La carte", "/pages/carte.html"),
+    new Route("/reservation", "Réservation", "/pages/reservation.html"),
+    new Route("/compte", "Mon compte", "/pages/compte.html"),
+    new Route("/login", "Connexion", "/pages/login.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
